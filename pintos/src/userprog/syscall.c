0758a65062cd61add0372c1c99ba89fd3af745f8
coding-fish <601343748@qq.com>
2020-11-20 22:02:11 +0800

    Compile some modules in examples

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -3,8 +3,46 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "devices/shutdown.h"
 
+#define STDIN 0
+#define STDOUT 1
+
+
+#define SYS_CALL_NUM 20
 static void syscall_handler (struct intr_frame *);
+
+/* Process identifier. */
+typedef int pid_t;
+
+void sys_halt(struct intr_frame* f); /* Halt the operating system. */
+void sys_exit(struct intr_frame* f); /* Terminate this process. */
+void sys_exec(struct intr_frame* f); /* Start another process. */
+void sys_wait(struct intr_frame* f); /* Wait for a child process to die. */
+void sys_create(struct intr_frame* f); /* Create a file. */
+void sys_remove(struct intr_frame* f);/* Create a file. */
+void sys_open(struct intr_frame* f); /*Open a file. */
+void sys_filesize(struct intr_frame* f);/* Obtain a file's size. */
+void sys_read(struct intr_frame* f);  /* Read from a file. */
+void sys_write(struct intr_frame* f); /* Write to a file. */
+void sys_seek(struct intr_frame* f); /* Change position in a file. */
+void sys_tell(struct intr_frame* f); /* Report current position in a file. */
+void sys_close(struct intr_frame* f); /* Close a file. */
+
+//Projects 2 and later.
+void halt (void) NO_RETURN;
+void exit (int status) NO_RETURN;
+pid_t exec (const char *cmd_line);
+int wait (pid_t);
+bool create (const char *file, unsigned initial_size);
+bool remove (const char *file);
+int open (const char *file);
+int filesize (int fd);
+int read (int fd, void *buffer, unsigned size);
+int write (int fd, const void *buffer, unsigned size);
+void seek (int fd, unsigned position);
+unsigned tell (int fd);
+void close (int fd);
 
 void
 syscall_init (void) 
@@ -15,6 +53,12 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  printf ("system call!\n");
+  //printf ("system call!\n");
+  // hex_dump();
   thread_exit ();
 }
+
+// void halt ()
+// {
+//   shutdown_power_off();
+// }
